58ec749011333954fd68729ba2e5704166692ddf
alexis bustamante <alexisbustamantecisternas@gmail.com>
2022-04-30 19:29:17 -0400

    cambio en algoritmo de contabilizar el tiempo desde que se creo la página

--- index.js
+++ index.js
@@ -1,40 +1,27 @@
-
 function carga() { 
 
-    contador_s = 0;
-    contador_m = 0;
-    contador_h = 0;
-    contador_d = 0;
+    let iniDate = new Date("April 27, 2022 12:10:00");
 
     s = document.getElementById('segundos');
     m = document.getElementById('minutos');
     h = document.getElementById('horas');
     d = document.getElementById('dias');
 
-
     window.setInterval(function() {
-        //console.log(contador_s);
+        let vendDate = new Date();
+        var timeMilisDif = vendDate.getTime() - iniDate.getTime();
 
-        if (contador_s==60) {
-            contador_m++;
-            m.innerHTML = contador_m < 10 ? "0" + contador_m : contador_m;
-            contador_s = 0;
-        }
+        var se = Math.round(timeMilisDif / 1000);
+        //console.log(secondsFormat(seconds));
+        var day = Math.floor(se / (24 * 3600)); // Math.floor () redondea hacia abajo 
+        var hour = Math.floor((se - day * 24 * 3600) / 3600);
+        var minute = Math.floor((se - day * 24 * 3600 - hour * 3600) / 60);
+        var second = se - day * 24 * 3600 - hour * 3600 - minute * 60; 
 
-        if (contador_m==60) {
-            contador_h++;
-            contador_m = 0;
-            h.innerHTML = contador_h < 10 ? "0" + contador_h : contador_h;
-        }
-
-        if (contador_h == 24) {
-            contador_d++;
-            contador_h = 0;
-            d.innerHTML = contador_d < 10 ? "0" + contador_d : contador_d;
-        }
-
-        s.innerHTML = contador_s < 10 ? "0" + contador_s : contador_s;
-        contador_s++;
+        s.innerHTML = second < 10 ? "0" + second : second;
+        m.innerHTML = minute < 10 ? "0" + minute : minute;
+        h.innerHTML = hour < 10 ? "0" + hour : hour;
+        d.innerHTML = day < 10 ? "0" + day : day;
 
     }, 1000)
     
